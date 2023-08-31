68eb412b0324123798fa18c7bc0b159a989571c3
popojk <c.alexwu@gmail.com>
2023-08-31 09:52:11 +0800

    create user model and mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { PostsController } from './posts/posts.controller';
+import { PostsModule } from './posts/posts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    UsersModule, 
+    PostsModule,
+    MongooseModule.forRoot('mongodb://root:password@localhost:27016/vinstagram?authSource=admin')
+  ],
+  //controllers: [AppController, UsersController, PostsController],
+  //providers: [AppService, UsersService],
 })
 export class AppModule {}
